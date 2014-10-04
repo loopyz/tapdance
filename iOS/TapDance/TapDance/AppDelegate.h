87f8cd72d06143b7456a4e683b8bb05625b54111
loopyz <lguo@andrew.cmu.edu>
2014-10-04 11:26:04 -0400

    TapDance initial commit

---
+++ iOS/TapDance/TapDance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TapDance
+//
+//  Created by Lucy Guo on 10/4/14.
+//  Copyright (c) 2014 TapDance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/TapDance/TapDance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TapDance
+//
+//  Created by Lucy Guo on 10/4/14.
+//  Copyright (c) 2014 TapDance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
